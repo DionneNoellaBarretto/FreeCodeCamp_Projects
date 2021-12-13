ea3b8aad3c33d701ea4fd74f144bb874cf03cf20
Dionne Noella Barretto <denverdionne@gmail.com>
2021-12-12 22:05:49 -0500

    dom exercises

--- JavaScript/initializing-variables-with-the-assignment-operator.js
+++ JavaScript/initializing-variables-with-the-assignment-operator.js
@@ -1,1 +1,7 @@
+/* Initializing Variables with the Assignment OperatorPassed
+It is common to initialize a variable to an initial value in the same line as it is declared.
+
+var myVar = 0;
+Creates a new variable called myVar and assigns it an initial value of 0.*/
+
 var a =9;
--- JavaScript/understanding-uninitialized-variables.js
+++ JavaScript/understanding-uninitialized-variables.js
@@ -1,3 +1,10 @@
+/* 
+Understanding Uninitialized VariablesPassed
+When JavaScript variables are declared, they have an initial value of undefined. If you do a mathematical operation on an undefined variable your result will be NaN which means "Not a Number". If you concatenate a string with an undefined variable, you will get a literal string of undefined.
+
+Initialize the three variables a, b, and c with 5, 10, and "I am a" respectively so that they will not be undefined.
+*/
+
 // Only change code below this line
 var a =5;
 var b =10;
