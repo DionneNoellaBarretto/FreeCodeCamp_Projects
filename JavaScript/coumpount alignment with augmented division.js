1c0587d9ec043aff6ebd473361b00c50c67f0470
Dionne Noella Barretto <denverdionne@gmail.com>
2021-12-13 21:41:41 -0500

    more exercises

--- JavaScript/coumpount alignment with augmented division.js
+++ JavaScript/coumpount alignment with augmented division.js
@@ -9,3 +9,11 @@ myVar /= 5;
 Convert the assignments for a, b, and c to use the /= operator.*/
 
 
+let a = 48;
+let b = 108;
+let c = 33;
+
+// Only change code below this line
+a = a / 12;
+b = b / 4;
+c = c / 11;
